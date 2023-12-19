cf4cecbd544730acbd1d102998eca0d4b7ca4a57
Patrik Santos <patrikogit@gmail.com>
2023-12-19 18:54:40 -0300

    Frontend teste alphacode

---
+++ README.md
@@ -0,0 +1,43 @@
+# Frontend Web Responsivo em HTML, CSS e jQuery - Contatos
+
+Este é o frontend de uma aplicação web responsiva desenvolvida com HTML, CSS e jQuery para interagir com a [API REST MVC de Contatos](https://github.com/kaitbunny/teste-backend-patrik). A aplicação foi projetada para funcionar de forma eficiente em dispositivos como celulares, tablets e PCs.
+
+## Estrutura do Projeto
+
+A estrutura do projeto é composta pelos seguintes arquivos:
+
+1. `index.html`: Página principal da aplicação.
+2. `style.css`: Arquivo de estilos para a formatação da interface.
+3. `script.js`: Script jQuery para interação com a API e manipulação da interface.
+
+## Configuração do Ambiente
+
+Para configurar e executar este frontend, siga os passos abaixo:
+
+1. Faça o download dos arquivos e coloque-os em um diretório de sua escolha.
+2. Abra o arquivo `index.html` em seu navegador web preferido.
+3. Certifique-se de que a [API REST MVC de Contatos](https://github.com/kaitbunny/teste-backend-patrik) esteja em execução e configurada corretamente.
+4. Mude a constante `baseUrl` no arquivo `script.js` para o endereço onde a API está rodando na sua máquina.
+
+## Funcionalidades
+
+A aplicação frontend oferece as seguintes funcionalidades, correspondentes aos métodos da API:
+
+- **Listar Contatos:** Exibe a lista de contatos cadastrados.
+- **Mostrar Dados de um Contato:** Apresenta os detalhes de um contato específico.
+- **Cadastrar Novo Contato:** Permite a adição de um novo contato.
+- **Editar Contato Existente:** Possibilita a atualização das informações de um contato.
+- **Excluir Contato Existente:** Remove um contato do sistema.
+
+## Testando o Frontend
+
+Utilize seu navegador para interagir com a aplicação e teste as diferentes funcionalidades. Certifique-se de que a [API](https://github.com/kaitbunny/teste-backend-patrik) esteja disponível e corretamente configurada para garantir a comunicação adequada entre o frontend e o backend.
+
+## Contato do Desenvolvedor
+
+- **Desenvolvedor:** Patrik Pereira dos Santos
+  - **LinkedIn:** [patrik santos](https://www.linkedin.com/in/patriksantos1/)
+
+---
+
+**Nota:** Este frontend foi desenvolvido para ser utilizado em conjunto com a [API REST MVC de Contatos](https://github.com/kaitbunny/teste-backend-patrik). Certifique-se de seguir as instruções de configuração da API para garantir a integração adequada entre as duas partes da aplicação.
---
+++ script.js
@@ -0,0 +1,338 @@
+$(document).ready(function(){
+  //altere para o endereco ipv4 e a porta do apache da sua máquina
+  const baseUrl = 'http://192.168.1.107:80/api_alphacode/'
+
+  //deixa o label azul
+  function deixarLabelAzul() {
+    $('.form-text-input input').focus(function () {
+        $(this).prev('label').css('color', '#068ed0');
+    }).blur(function () {
+        $(this).prev('label').css('color', '');
+    });
+  }
+
+  function aplicarMascaras() {
+    $('.data').mask('00/00/0000');
+    $('.telefone').mask('(00) 0000-0000');
+    $('.celular').mask('(00) 00000-0000');
+  }
+  
+  function configurarMetodosDeValidacao() {
+    $.validator.methods.date = function(value, element) {
+      var regex = /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[012])\/(19[0-9]{2}|20[0-9]{2}|210[0-5])$/;
+      return this.optional(element) || regex.test(value);
+    };
+  
+    $.validator.methods.cell = function(value, element) {
+      var regex = /^\(\d{2}\) \d{5}-\d{4}$/;
+      return this.optional(element) || regex.test(value);
+    };
+  }
+  
+  function configurarValidacaoDoFormulario() {
+    $(".main-form").validate({
+      rules: {
+        nome: {
+          minlength: 2
+        },
+        data: {
+          date: true
+        },
+        email: {
+          email: true
+        },
+        profissao: {
+          minlength: 2
+        },
+        celular: {
+          cell: true
+        }
+      },
+      messages: {
+        nome: {
+          required: "Por favor, digite seu nome!",
+          minlength: "O nome precisa ter no mínimo duas letras!"
+        },
+        data: "Por favor, digite uma data válida no formato dd/mm/yyyy!",
+        profissao: {
+          required: "Por favor, digite sua profissão!",
+          minlength: "O nome da profissão precisa ter no mínimo duas letras!"
+        },
+        email: "Por favor, digite seu E-mail!",
+        celular: "Por favor, digite um número de celular válido com 11 dígitos!"
+      },
+      submitHandler: function(form) {
+        form.submit();
+      }
+    });
+  }
+
+  deixarLabelAzul();
+  aplicarMascaras();
+  configurarMetodosDeValidacao();
+  configurarValidacaoDoFormulario();
+
+  //faz a requisição para o método listar da api
+  function carregarContatos() {
+    $.ajax({
+      url: baseUrl + 'contatos/listar',
+      method: 'GET',
+      dataType: 'json',
+      success: function (data) {
+        if (data.tipo === 'sucesso' && data.resposta) {
+          $('#tabela-contatos').find('tr:gt(0)').remove();
+          data.resposta.forEach(adicionarLinhaTabela);
+        }
+      },
+      error: function (error) {
+        console.error('Erro na requisição AJAX:', error);
+      }
+    });
+  }
+
+  //métodos para formatar os dados exibidos da tabela
+  function formatarData(data) {
+    return moment(data, 'YYYY-MM-DD').format('DD/MM/YYYY');
+  }
+
+  function formatarCelular(celular) {
+    return celular.replace(/(\d{2})(\d{5})(\d{4})/, '($1) $2-$3');
+  }
+
+  function formatarTelefone(telefone) {
+    return telefone.replace(/(\d{2})(\d{4,5})(\d{4})/, '($1) $2-$3');
+  }
+
+  //adiciona uma linha da tabela
+  function adicionarLinhaTabela(contato) {
+    var tabela = $('#tabela-contatos');
+    var linha = $('<tr>').data('id', contato.id).append(
+      $('<td>').text(contato.nome),
+      $('<td>').text(formatarData(contato.nascimento)),
+      $('<td>').text(contato.email),
+      $('<td>').text(formatarCelular(contato.celular)),
+      $('<td>').html('<div class="tabela-options">' +
+        '<button class="editar-contato" data-id="' + contato.id + '"><img src="assets/editar.png" alt="editar"></button>' +
+        '<button class="excluir-contato" data-id="' + contato.id + '"><img src="assets/excluir.png" alt="excluir"></button>' +
+        '</div>')
+    );
+    tabela.append(linha);
+  }
+
+  //faz o cadastro de um contato
+  $('#form-cadastro').submit(function (event) {
+    event.preventDefault();
+
+    //formata os dados do formulario para enviar o json
+    var formDataInsert = {
+      nome: $('#nome').val(),
+      nascimento: moment($('#data').val(), 'DD/MM/YYYY').format('YYYY-MM-DD'),
+      email: $('#email').val(),
+      profissao: $('#profissao').val(),
+      telefone: $('#telefone').val().replace(/\D/g, ''),
+      celular: $('#celular').val().replace(/\D/g, ''),
+      celularWhatsapp: $('#whatsapp').prop('checked'),
+      recebeEmail: $('#emailnotif').prop('checked'),
+      recebeSms: $('#sms').prop('checked')
+    };
+
+    //ajusta os valores do checkbox
+    $.each(formDataInsert, function (key, value) {
+      if (typeof value === 'boolean' && !value) {
+        formDataInsert[key] = false;
+      }
+    });
+
+    //requisição do método cadastrar na api
+    $.ajax({
+      url: baseUrl + 'contatos/cadastrar',
+      method: 'POST',
+      contentType: 'application/json',
+      data: JSON.stringify(formDataInsert),
+      success: function (response) {
+        carregarContatos();
+      },
+      error: function (error) {
+        //popup erro
+        exibirPopupErro("Cadastro de contato não realizado. Por favor, tente novamente!");
+        console.error('Erro ao cadastrar contato:', error);
+        carregarContatos();
+      }
+    });
+
+    return false;
+  });
+
+  //faz um popup de erro
+  function exibirPopupErro(mensagem) {
+    Swal.fire('Erro!', mensagem, 'error');
+  }
+
+  //faz a exclusao de um contato
+  $(document).on('click', '#tabela-contatos button.excluir-contato', function () {
+    var linha = $(this).closest('tr');
+    var nomeContato = linha.find('td:first').text();
+
+    Swal.fire({
+      title: 'Confirmar exclusão',
+      text: 'Deseja realmente excluir o contato ' + nomeContato + '?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#068ed0',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Sim, excluir!',
+      cancelButtonText: 'Não, cancelar'
+    }).then((result) => {
+      if(result.isConfirmed) {
+        var contatoId = linha.data('id');
+
+        $.ajax({
+          url: baseUrl + 'contatos/excluir/' + contatoId,
+          method: 'DELETE',
+          success: function (response) {
+            console.log('Contato excluído com sucesso:', response);
+            linha.remove();
+            Swal.fire('Sucesso!', 'O contato foi excluído.', 'success');
+          },
+          error: function (error) {
+            console.error('Erro ao excluir contato:', error);
+            exibirPopupErro('Ocorreu um erro ao excluir o contato.');
+          }
+        });
+      }
+    });
+  });
+
+  //faz a requisição para o método editar da api
+  $(document).on('click', '#tabela-contatos button.editar-contato', function () {
+    var contatoId = $(this).data('id');
+
+    $.ajax({
+      url: baseUrl + 'contatos/mostrar/' + contatoId,
+      method: 'GET',
+      dataType: 'json',
+      success: function (data) {
+        if (data.tipo === 'sucesso' && data.resposta) {
+          abrirPopupEdicao(contatoId);
+          preencherFormulario(data.resposta);
+        }
+      },
+      error: function (error) {
+        exibirPopupErro("Erro ao solicitar edição para o servidor. Por favor, tente novamente mais tarde!");
+      }
+    });
+  });
+
+  //preenche o formulário de edição com os dados atuais do contato
+  function preencherFormulario(contato) {
+    aplicarMascaras();
+    configurarMetodosDeValidacao();
+    configurarValidacaoDoFormulario();
+    deixarLabelAzul();
+
+    $('#edit-nome').val(contato.nome);
+    $('#edit-data').val(moment(contato.nascimento).format('DD/MM/YYYY'));
+    $('#edit-email').val(contato.email);
+    $('#edit-profissao').val(contato.profissao);
+    $('#edit-telefone').val(formatarTelefone(contato.telefone));
+    $('#edit-celular').val(formatarCelular(contato.celular));
+    $('#edit-whatsapp').prop('checked', contato.celularWhatsapp);
+    $('#edit-emailnotif').prop('checked', contato.recebeEmail);
+    $('#edit-sms').prop('checked', contato.recebeSms);
+  }
+
+  //abre o pop up de edição e caso o usuário clique em editar ele faz a requisição para o método editar da api
+  function abrirPopupEdicao(contatoId) {
+    Swal.fire({
+      title: 'Editar Contato',
+      width: 1000,
+      html: `
+        <main class="container no-margin-bottom">
+        <form id="form-atualizar" class="main-form">
+          <div class="form-text-input">
+            <label for="edit-nome">Nome completo</label>
+            <input type="text" id="edit-nome" name="nome" placeholder="Ex.: Patrik Pereira dos Santos" required>
+          </div>
+          <div class="form-text-input">
+            <label for="edit-data">Data de nascimento</label>
+            <input class="data" type="text" id="edit-data" name="data" placeholder="Ex.: 12/05/2003" required>
+          </div>
+          <div class="form-text-input">
+            <label for="edit-email">E-mail</label>
+            <input type="email" id="edit-email" name="email" placeholder="Ex.: patrik@gmail.com" required>
+          </div>
+          <div class="form-text-input">
+            <label for="edit-profissao">Profissão</label>
+            <input type="text" id="edit-profissao" name="profissao" placeholder="Ex.: Desenvolvedor Backend" required>
+          </div>
+          <div class="form-text-input">
+            <label for="edit-telefone">Telefone para contato</label>
+            <input class="telefone" type="text" id="edit-telefone" name="telefone" placeholder="Ex.: (11) 4002-8922">
+          </div>
+          <div class="form-text-input">
+            <label for="edit-celular">Celular para contato</label>
+            <input class="celular" type="text" id="edit-celular" name="celular" placeholder="Ex.: (11) 98493-2039" required>
+          </div>
+          <div class="form-checkbox-input margin-top">
+            <input type="checkbox" id="edit-whatsapp" name="whatsapp">
+            <label for="edit-whatsapp">Número de celular possui Whatsapp</label>
+          </div>
+          <div class="form-checkbox-input margin-top">
+            <input type="checkbox" id="edit-emailnotif" name="emailnotif">
+            <label for="edit-emailnotif">Enviar notificações por E-mail</label>
+          </div>
+          <div class="form-checkbox-input">
+            <input type="checkbox" id="edit-sms" name="sms">
+            <label for="edit-sms">Enviar notificações por SMS</label>
+          </div>
+        </form>
+      </main>
+      `,
+      showCancelButton: true,
+      cancelButtonText: 'Cancelar',
+      confirmButtonText: 'Atualizar Contato',
+      preConfirm: function () {
+          var formDataUpdate = {
+            nome: $('#edit-nome').val(),
+            nascimento: moment($('#edit-data').val(), 'DD/MM/YYYY').format('YYYY-MM-DD'),
+            email: $('#edit-email').val(),
+            profissao: $('#edit-profissao').val(),
+            telefone: $('#edit-telefone').val().replace(/\D/g, ''),
+            celular: $('#edit-celular').val().replace(/\D/g, ''),
+            celularWhatsapp: $('#edit-whatsapp').prop('checked'),
+            recebeEmail: $('#edit-emailnotif').prop('checked'),
+            recebeSms: $('#edit-sms').prop('checked')
+          };
+  
+          $.each(formDataUpdate, function (key, value) {
+            if (typeof value === 'boolean' && !value) {
+              formDataUpdate[key] = false;
+            }
+          });
+          
+          return $.ajax({
+            url: baseUrl + 'contatos/editar/' + contatoId,
+            method: 'PUT',
+            contentType: 'application/json',
+            data: JSON.stringify(formDataUpdate),
+            success: function (result) {
+              if(result.tipo === 'sucesso') {
+                Swal.fire('Sucesso!', 'O contato foi atualizado com sucesso.', 'success').then(() => {
+                  location.reload(true)
+                });
+              }
+              else {
+                exibirPopupErro('O contato não foi atualizado.');
+              }
+            },
+            error: function (error) {
+              exibirPopupErro('Ocorreu um erro durante a atualização do contato.');
+            }
+          });
+        } 
+    })
+  }
+
+  //carrega os contatos
+  carregarContatos();
+});
